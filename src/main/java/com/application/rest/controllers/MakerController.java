dfb0d6cd6690a290fb33d9f2713d93d73f64e989
tizianaamica <tiziana.amicarella@unet.edu.ve>
2024-03-05 01:04:25 +0100

    added new endpoint to maker

--- src/main/java/com/application/rest/controllers/MakerController.java
+++ src/main/java/com/application/rest/controllers/MakerController.java
@@ -6,11 +6,11 @@ import com.application.rest.entities.Maker;
 import com.application.rest.service.IMakerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -37,6 +37,54 @@ public class MakerController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll(){
+        List<MakerDTO> makerList = makerService.findAll()
+                .stream()
+                .map(maker -> MakerDTO.builder()
+                        .id(maker.getId())
+                        .name(maker.getName())
+                        .products(maker.getProducts())
+                        .build())
+                .toList();
+        return ResponseEntity.ok(makerList);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody MakerDTO makerDTO) throws URISyntaxException {
+        if(makerDTO.getName().isBlank()){
+            return ResponseEntity.badRequest().build();
+        }
+        makerService.save(Maker.builder()
+                        .name(makerDTO.getName())
+                        .build());
+        return ResponseEntity.created(new URI("/api/makers/save")).build();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateMaker(@PathVariable Long id, @RequestBody MakerDTO makerDTO){
+        Optional<Maker> makerOptional = makerService.findById(id);
+
+        if (makerOptional.isPresent()) {
+            Maker maker = makerOptional.get();
+            maker.setName(makerDTO.getName());
+            makerService.save(maker);
+            return ResponseEntity.ok("Updated maker");
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id) {
+        if(id != null) {
+            makerService.deleteById(id);
+            return ResponseEntity.ok("Deleted");
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 
 }
